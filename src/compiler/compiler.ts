21057e35e2df6d2d85b7f1bdd5c767ae8b3edf0c
Pouya Kary <k@karyfoundation.org>
2016-11-14 16:21:10 +0330

    main compiler's body...

---
+++ src/compiler/compiler.ts
@@ -0,0 +1,32 @@
+
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+namespace numeraX.compiler {
+
+    //
+    // ─── MAIN ───────────────────────────────────────────────────────────────────────
+    //
+
+        export function generate ( node: jsep.interfaces.baseNode ): string {
+            switch ( node.type ) {
+                case 'Literal':
+                    return generators.compileLiteralNode( <jsep.interfaces.literalNode> node )
+
+                case 'Identifier':
+                    return generators.compileIdentifierNode( <jsep.interfaces.identiferNode> node )
+
+                case 'BinaryExpression':
+                    return generators.compileBinaryExpressionNode( <jsep.interfaces.binaryExpressionNode> node )
+            }
+        }
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
