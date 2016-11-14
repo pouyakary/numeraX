d86900bae7a9b891e92b827c15ad3645f9a35d4d
Pouya Kary <k@karyfoundation.org>
2016-11-14 16:25:13 +0330

    compiler's main...

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+namespace numeraX {
+
+    //
+    // ─── THE VERY MAIN ──────────────────────────────────────────────────────────────
+    //
+
+        // Compiles a simple math expression into TeX mathematical expression natation.
+        export function compile ( code: string ): string {
+            parser.setupJSEP( );
+
+            try {
+                // getting the ast
+                let ast = jsep( code );
+
+                // compiling the ast into TeX
+                return compiler.generate( ast );
+
+            } catch ( error ) {
+                return null;
+            }
+        }
+
+    // ────────────────────────────────────────────────────────────────────────────────
+
+}
